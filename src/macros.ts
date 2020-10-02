6ddd6756845521255640ea5562a0d17c6e01be1c
Ben Gubler <nebrelbug@gmail.com>
2020-10-01 20:02:59 -0600

    Remove block, layout functions

--- src/macros.ts
+++ src/macros.ts
@@ -3,10 +3,6 @@ var captureRegEx = /{@|@}/g
 export function macros() {
   return {
     processAST: function (buff: Array<any>, config: { varName: string; [index: string]: any }) {
-      buff.unshift({
-        t: 'e',
-        val: `function block(t,n){var i=${config.varName}[t];return i?"function"==typeof i?i(${config.varName}):i:"function"==typeof n?n(${config.varName}):n}function layout(t,n){if(n)return E.includeFile(t,n);tR.l=t}`
-      })
       for (var i = 0; i < buff.length; i++) {
         let currItem = buff[i]
         if (captureRegEx.test(currItem.val)) {
@@ -20,42 +16,7 @@ export function macros() {
         }
       }
 
-      buff.push({
-        t: 'e',
-        val: 'if(tR.l)tR=E.includeFile(tR.l,tR)'
-      })
-
       return buff
     }
   }
 }
-
-/*
-Here's the code for the block() and layout() functions
-(before they are minified and `it` is replaced with config.varName)
-=========================
-function block(name, fallback) {
-  // Content passed to the block
-  var blockContent = it[name]
-  if (blockContent) {
-    if (typeof blockContent === "function") {
-      return blockContent(it)
-    } else {
-      return blockContent
-    }
-  } else if (typeof fallback === "function") {
-    return fallback(it)
-  } else {
-    return fallback
-  }
-}
-
-function layout(path, content) {
-  if (content) {
-    return E.includeFile(path, content)
-  } else {
-    tR.l = path
-  }
-}
-
-*/
